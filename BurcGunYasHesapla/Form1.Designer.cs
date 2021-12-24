a89442645d169e35ef85245b0cbac4ceb2423318
Galip Yıldız <galipyildiz9606@outlook.com>
2021-12-24 16:23:26 +0300

    Add project files.

---
+++ BurcGunYasHesapla/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace BurcGunYasHesapla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpDogumTarihi = new System.Windows.Forms.DateTimePicker();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblBurc = new System.Windows.Forms.Label();
+            this.lblGun = new System.Windows.Forms.Label();
+            this.lblYas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Doğum Tarihiniz";
+            // 
+            // dtpDogumTarihi
+            // 
+            this.dtpDogumTarihi.Location = new System.Drawing.Point(12, 61);
+            this.dtpDogumTarihi.Name = "dtpDogumTarihi";
+            this.dtpDogumTarihi.Size = new System.Drawing.Size(398, 35);
+            this.dtpDogumTarihi.TabIndex = 1;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(128, 117);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(178, 51);
+            this.btnHesapla.TabIndex = 2;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblBurc
+            // 
+            this.lblBurc.AutoSize = true;
+            this.lblBurc.Location = new System.Drawing.Point(70, 302);
+            this.lblBurc.Name = "lblBurc";
+            this.lblBurc.Size = new System.Drawing.Size(118, 29);
+            this.lblBurc.TabIndex = 0;
+            this.lblBurc.Text = "Burcunuz:";
+            // 
+            // lblGun
+            // 
+            this.lblGun.AutoSize = true;
+            this.lblGun.Location = new System.Drawing.Point(70, 252);
+            this.lblGun.Name = "lblGun";
+            this.lblGun.Size = new System.Drawing.Size(193, 29);
+            this.lblGun.TabIndex = 0;
+            this.lblGun.Text = "Yaşadığınız Gün:";
+            // 
+            // lblYas
+            // 
+            this.lblYas.AutoSize = true;
+            this.lblYas.Location = new System.Drawing.Point(70, 196);
+            this.lblYas.Name = "lblYas";
+            this.lblYas.Size = new System.Drawing.Size(96, 29);
+            this.lblYas.TabIndex = 0;
+            this.lblYas.Text = "Yaşınız:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(434, 408);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.dtpDogumTarihi);
+            this.Controls.Add(this.lblYas);
+            this.Controls.Add(this.lblGun);
+            this.Controls.Add(this.lblBurc);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Burç Gün Yaş Hesapla";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpDogumTarihi;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblBurc;
+        private System.Windows.Forms.Label lblGun;
+        private System.Windows.Forms.Label lblYas;
+    }
+}
+
