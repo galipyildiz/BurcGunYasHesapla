53d69fa12b7de8ab00bcee60e6bdc9eb06bbadbe
Galip Yıldız <galipyildiz9606@outlook.com>
2021-12-24 16:41:24 +0300

    Yas hesaplama ozelligi eklendi.

--- BurcGunYasHesapla/Form1.cs
+++ BurcGunYasHesapla/Form1.cs
@@ -12,14 +12,27 @@ namespace BurcGunYasHesapla
 {
     public partial class Form1 : Form
     {
+        DateTime dogumTarihi;
         public Form1()
         {
             InitializeComponent();
+            Hesapla();
+        }
+
+        private void Hesapla()
+        {
+            dogumTarihi = dtpDogumTarihi.Value;
+            lblYas.Text = "Yaşınız: " + YasHesapla(dogumTarihi.Year);
+        }
+
+        private string YasHesapla(int year)
+        {
+            return (DateTime.Now.Year - year).ToString();
         }
 
         private void btnHesapla_Click(object sender, EventArgs e)
         {
-            
+            Hesapla();
         }
     }
 }
