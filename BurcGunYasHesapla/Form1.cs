1dc3589b9abd3636c6b504b8ef5690e10fc68a00
Galip Yıldız <galipyildiz9606@outlook.com>
2021-12-24 17:09:44 +0300

    Burc ozellegi eklendi.

--- BurcGunYasHesapla/Form1.cs
+++ BurcGunYasHesapla/Form1.cs
@@ -24,6 +24,61 @@ namespace BurcGunYasHesapla
             dogumTarihi = dtpDogumTarihi.Value;
             lblYas.Text = "Yaşınız: " + YasHesapla(dogumTarihi.Year);
             lblGun.Text = "Yaşadığınız Gün: " + GunHesapla(dogumTarihi);
+            lblBurc.Text = "Burcunuz: " + BurcHesapla(dogumTarihi.Month, dogumTarihi.Day);
+        }
+
+        private string BurcHesapla(int month, int day)
+        {
+            string burc = "";
+            if ((month == 1 && day >= 21) || (month == 2 && day <= 19))//Kova => Ocak 21 - 19 Şubat
+            {
+                burc = "Kova";
+            }
+            else if ((month == 2 && day >= 20) || (month == 3 && day <= 20))//Balık => 20 Şubat - 20 Mart
+            {
+                burc = "Balık";
+            }
+            else if ((month == 3 && day >= 21) || (month == 4 && day <= 20))//Koç => 21 Mart - 20 Nisan
+            {
+                burc = "Koç";
+            }
+            else if ((month == 4 && day >= 21) || (month == 5 && day <= 21))//Boğa => 21 Nisan - 21 Mayıs
+            {
+                burc = "Boğa";
+            }
+            else if ((month == 5 && day >= 22) || (month == 6 && day <= 21))//İkizler => 22 Mayıs - 21 Haziran
+            {
+                burc = "İkizler";
+            }
+            else if ((month == 6 && day >= 22) || (month == 7 && day <= 23))//Yengeç => 22 Haziran - 23 Temmuz
+            {
+                burc = "Yengeç";
+            }
+            else if ((month == 7 && day >= 24) || (month == 8 && day <= 23))//Aslan => 24 Temmuz - 23 Ağustos
+            {
+                burc = "Aslan";
+            }
+            else if ((month == 8 && day >= 24) || (month == 9 && day <= 22))//Başak => 24 Ağustos - 22 Eylül
+            {
+                burc = "Başak";
+            }
+            else if ((month == 9 && day >= 23) || (month == 10 && day <= 23))//Terazi => 23 Eylül - 23 Ekim
+            {
+                burc = "Terazi";
+            }
+            else if ((month == 10 && day >= 24) || (month == 11 && day <= 22))//Akrep => 24 Ekim - 22 Kasım
+            {
+                burc = "Akrep";
+            }
+            else if ((month == 11 && day >= 23) || (month == 12 && day <= 21))//Yay => 23 Kasım - 21 Aralık
+            {
+                burc = "Yay";
+            }
+            else//Oğlak => 22 Aralık - 20 Ocak
+            {
+                burc = "Oğlak";
+            }
+            return burc;
         }
 
         private string GunHesapla(DateTime dogumTarihi)
